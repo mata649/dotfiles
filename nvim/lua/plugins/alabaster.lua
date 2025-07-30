6975df6830b238796817d6395ab2064b7e94d438
Jose Mata <mata649@hotmail.com>
2025-07-29 20:55:47 -0600

    deactivate alabaster

---
+++ nvim/lua/plugins/alabaster.lua
@@ -0,0 +1,9 @@
+return {
+    "p00f/alabaster.nvim",
+    name = "alabaster",
+    lazy = false,      -- Load immediately
+    priority = 1000,   -- Ensure it loads before other plugins that might depend on it
+    config = function ()
+       vim.cmd("colorscheme alabaster") 
+    end
+}
